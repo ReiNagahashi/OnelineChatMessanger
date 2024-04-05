07c03a1594a4dd50abd2b0cb6ebd1eb2af3b0285
Hashi <rerenaga9170@icloud.com>
2024-04-05 17:32:48 +0900

    Communication with validation
    Parameters can include user_name or messages on each situation.

--- client.py
+++ client.py
@@ -1,7 +1,27 @@
 import socket
+import select
+import sys
 import json
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
+
+
+def create_bytes_data(command, parameters):
+    data = {
+        "command": command,
+        "parameters": parameters
+    }
+
+    return json.dumps(data).encode('utf-8')
+
+
+
+def close_socket_with_error():
+    print('Error occured! Socket Closed...')
+    sock.close()
+    sys.exit(1)
+
+
 
 server_ip = input("Type in the server's ip address to connect to: ")
 server_port = int(input("Type in the server's port number to connect to: "))
@@ -11,20 +31,65 @@ server_address = (server_ip, server_port)
 address = ''
 port = 9050
 
-user_name = input("What's your name?")
-msg = input("Enter your message: ")
-
-data = {"user_name": user_name, "message": msg}
-bytes_data = json.dumps(data).encode('utf-8')
-
 sock.bind((address, port))
 
+# ユーザー名の入力
+while True:
+    user_name = input("What's your name?")
+    data = create_bytes_data("user_name", {'user_name': user_name})
+    try:
+        sent = sock.sendto(data, server_address)
+        print(f"Sent your name, wait for response")
+        # Welcome! というレスポンスが来たら成功
+        response, server = sock.recvfrom(4096)
+        json_response = json.loads(response)
+        # エラーがなければメッセージ入力に進める。何らかのエラーがあったら再度入力からやり直す
+        print(json_response["message"])
+        if json_response["no_error"] == True:
+            break
+    # 致命的なエラーの場合はソケットを閉じる関数を実行
+    except:
+        close_socket_with_error()
 
-try:
-    print('sending {!r}'.format(bytes_data))
-    sent = sock.sendto(bytes_data, server_address)
-    response, server = sock.recvfrom(4096)
-    print(f"received {response} from {server}")
-finally:
-    print('closing socket')
-    sock.close()
+sock.setblocking(0) #ソケットをノンブロッキングモードに設定
+
+logged_in = True
+
+# タイムアウト=クライアントがリレーシステムから削除され、サーバーから「出ていけ」というレスポンスが来て、そのままソケットを閉じる処理をする
+
+# プロンプトを表示
+print("Type messages: ", end='')
+sys.stdout.flush()  # stdoutをフラッシュして、プロンプトがすぐに表示されるようにする
+
+# メッセージの入力
+while logged_in:
+    # 入力待ちとソケットからのデータまちをselectで同時に行う 
+    # select関数の引数においたイテラブルな値を設定したタイムの間待つ。今回の場合readableの値がsockの場合はサーバからのレスポンス、sys.stdinの場合はユーザの入力。それぞれを設定したタイムだけ待つ。これらがどちらもタイムアウトしたらパスする
+    readable, writable, exceptional = select.select([sock, sys.stdin], [], [], 5.0)
+    if not (readable or writable or exceptional):
+        # タイムアウトしても何もしない。しかし実際にはサーバ側でlogged_inがFalseになるようなレスポンスを渡す
+        pass
+    else:
+        for s in readable:
+            # 自身が送ったメッセージを含めてクライアントのメッセージが届いている場合
+            if s is sock:
+                response, server = sock.recvfrom(4096)
+                json_response = json.loads(response)
+                if  json_response["is_expired"]:
+                    print("Time expired: Socket closed...")
+                    logged_in = False
+
+                print(json_response["message"])
+
+                # else:
+                #     # 別のクライアントのメッセージが表示される
+                #     print(response[1])
+            # 入力データが存在する場合
+            elif s is sys.stdin:
+                msg = sys.stdin.readline().strip()
+                data = create_bytes_data('message', {'message': msg})
+
+                sock.sendto(data, server_address)
+
+
+sock.close()
--- server.py
+++ server.py
@@ -1,5 +1,34 @@
+# ユーザ名を
 import socket
 import json
+from collections import defaultdict
+
+response = {
+    'no_error': True,
+    'message': '',
+    'is_expired': False
+}
+
+# (ip_address, port_number): (user_name->string, chatted_last_time->string?)
+# 時刻はm単位で登録する
+# online_users = {}
+# (ip_address, port_number): [時刻の文字列要素1, 時刻の文字列要素2, ...]
+# messages = defaultdict(list)
+# 第一引数："parameters"のディクショナリ, 第二引数：parametersの各要素の制限メモリを持ったリスト
+def data_size_validation(parameters, limits):
+    # ユーザー名は255bytes, 'message'であれば4096bytesを最大にしてバリデーションをする
+    param_sizes = parameters.values()
+    for param_size, limit in zip(param_sizes, limits):
+        # バイトサイズで比較するために、バイト型に変更
+        if len(param_size.encode('utf-8')) > limit:
+            response['no_error'] = False
+            response['message'] = "Error: One of parameters exceeds the limitation! Try again."
+            return False
+
+    response['no_error'] = True
+    print("Data size validation Succeeded!!")
+    return True
+
 
 sock = socket.socket(socket.AF_INET, socket.SOCK_DGRAM)
 
@@ -9,13 +38,23 @@ print('starting up on port {}'.format(server_port))
 
 sock.bind((server_address, server_port))
 
+
+
 while True:
     print('\nwaiting to receive message')
     data, address = sock.recvfrom(4096)
+    
     json_data = json.loads(data)
-    print(f"Received data: {json_data} from {address}")
-    if data:
-        response = b"Received your message!"
-        sent = sock.sendto(response, address)
+
+    if json_data['command'] == 'user_name': 
+        if data_size_validation(json_data['parameters'], [255]):
+            response['message'] = "Welcome to our message service"
+            # 登録ユーザーを格納する辞書に保存, メッセージは空白で初期化
+            # online_users[address] = (json_data['parameters']['user_name'])
+    elif json_data['command'] == 'message':
+        if data_size_validation(json_data['parameters'], [4096]):
+            response['message'] = "Your message is accepted:)"
+
+    sent = sock.sendto(json.dumps(response).encode('utf-8'), address)
 
 
